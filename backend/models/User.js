28cdbb0130bce709a5aa362d40c4b1824370c34a
abhinav-973 <vabhinav898@gmail.com>
2026-01-15 22:43:19 +0530

    update backend

---
+++ backend/models/User.js
@@ -0,0 +1,25 @@
+import mongoose from "mongoose";
+
+const userSchema = new mongoose.Schema(
+  {
+    name: {
+      type: String,
+      required: true,
+      trim: true,
+    },
+
+    email: {
+      type: String,
+      required: true,
+      unique: true,
+      lowercase: true,
+    },
+    password: {
+      type: String,
+      requied: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export default mongoose.model("User", userSchema); 
--- src/services/dashboardService.js
+++
@@ -1,9 +0,0 @@
-import React from 'react'
-import axios from 'axios'
-
-const API_URL = "http://localhost:5000";
-
-export async function fetchDashboardData() {
-    const response = await axios.get(`${API_URL}/dashboard/summary`);
-    return response.data;
-}
